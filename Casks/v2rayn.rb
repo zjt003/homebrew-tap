80d9e8b70e0143cb9e0da25c4b46c7d3b85a573d
zero <Z@0000301.xyz>
2025-02-14 14:33:28 +0800

    Update v2rayn.rb

--- Casks/v2rayn.rb
+++ Casks/v2rayn.rb
@@ -1,9 +1,9 @@
 cask "v2rayn" do
   arch arm: "arm64", intel: "64"
 
-  version "7.8.0"
-  sha256 arm:   "4516f86b39f14edfd5b166dc4bb82e605820e9188259facff9f274cfb3b2903b",
-         intel: "0ef45ae048a90e34adb35addf4797307b52cc8e2859e31a7e8c680e8bb56be27"
+  version "7.8.3"
+  sha256 arm:   "d0abf9c299982eb71c889d1cb96d334240c4f64c68ed3fe3dba22f36c42fd0d9",
+         intel: "56245d9cdc3836308d2e2e9fee37e6e9b3d37e01502b183353d0c9618041dcc7"
 
   url "https://github.com/2dust/v2rayN/releases/download/#{version}/v2rayN-macos-#{arch}.dmg",
       verified: "github.com/2dust/v2rayN"
